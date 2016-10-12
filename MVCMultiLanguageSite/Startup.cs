751fbe4da444ef20ef3a7b7af4457312b3b38a8a
Chien Vu <chien.vh@gmail.com>
2016-10-12 14:14:46 +0700

    Initial commit

---
+++ MVCMultiLanguageSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCMultiLanguageSite.Startup))]
+namespace MVCMultiLanguageSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
